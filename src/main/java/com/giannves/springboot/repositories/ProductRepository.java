f4070d0e418c28fa007d7d57e017217afd8e13c1
Gian Neves <gianneves@hotmail.com>
2023-11-16 10:21:57 -0300

    feat: Repository creation with JpaRepository

---
+++ src/main/java/com/giannves/springboot/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.giannves.springboot.repositories;
+
+
+import com.giannves.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
